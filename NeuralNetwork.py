732129577dda4cc81d7ea128a688ead0edb83ff1
Thales Mendes <thalesportomendes@hotmail.com>
2019-10-06 11:51:35 -0300

    add neural network

---
+++ NeuralNetwork.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import __future__ #for future features in newer versions
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# load database
+test = pd.read_csv("dadosFiltrado.csv")
+
+# choose train and test dataset
+train_dataset = test.sample(frac=0.75,random_state=1) #the random_state gives the seed for the randomization
+test_dataset = test.drop(train_dataset.index)
+
+# answers for train and test dataset
+train_labels = train_dataset.pop('label')
+test_labels = test_dataset.pop('label')
+
+def build_model():
+  model = keras.Sequential([
+      layers.Input(len(train_dataset.keys())), #input_shape = 5
+      layers.Dense(4, activation = 'relu'),
+      layers.Dense(2, activation='softmax'),
+  ])
+
+  model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+  return model
+
+model = build_model()
+print(model.summary())
+
+predicted_labels_ANN = model.fit(train_dataset, train_labels, epochs=4)
+print(predicted_labels_ANN)
+
+test_loss, test_acc = model.evaluate(test_dataset,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
